0cfcbfc51cc08662b980623edcea2a6e950fe07c
Konstantin <laPosteGenerale@gmail.com>
2022-02-17 14:50:36 +0500

    Update Python.py

--- Scripts/Python.py
+++ Scripts/Python.py
@@ -1,11 +1,15 @@
 from win32com import client
 
 def Foo():
+    # Connect to our COM-object
     lib = client.Dispatch('MyCOMobject')
+    # Use ArrayList is a proper type of argument for Dot-method of COM-object
     values = client.Dispatch('System.Collections.ArrayList')
     values.Add(10)
-    values.Add(2.3)
-    return lib.dot(values)
+    values.Add(145)
+    values.Add(0.3)
+    # Call Dot-method
+    return lib.Dot(values)
 
 if __name__ == "__main__":
     print(Foo())
