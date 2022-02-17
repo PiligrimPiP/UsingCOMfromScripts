b941a8c5717f400f991994eb4a338a07311a6b92
PiligrimPiP <laPosteGenerale@gmail.com>
2022-02-17 14:13:43 +0500

    Добавьте файлы проекта.

---
+++ COMDLL/MyCOMObject.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace CSCOMServer
+{
+    [ComVisible(true)]  // This is mandatory.
+    [Guid("36E6BC94-308C-4952-84E6-109041990EF7")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ProgId("MyCOMobject")]
+    public class MyCOMObject
+    {
+        #region Constructors 
+        public MyCOMObject()
+        {
+            Console.WriteLine("MyCOMobject is connected");
+        }
+
+        ~MyCOMObject()
+        {
+        }
+        #endregion
+
+        #region Public methods
+        public void Foo(string Message)
+        {
+            Console.WriteLine(PrintLine()+ MethodBase.GetCurrentMethod().Name);
+            if (!string.IsNullOrEmpty(Message)) 
+                Console.WriteLine("Message recieved: {0}", Message);
+
+        }
+        public void Foo()
+        {
+            Foo("");
+        }
+
+        /// <summary>
+        /// Multiply all numbers listed as ArrayList
+        /// </summary>
+        /// <param name="inputValues">System.Collections.ArrayList </param>
+        /// <returns>double</returns>
+        public double Dot(System.Collections.ArrayList inputValues)
+        {
+            double res = 1;
+            for (int i = 0; i < inputValues.Count; i++)
+            {
+                res = res * Convert.ToDouble(inputValues[i]);
+            }
+            return res;
+        }
+
+        public void ShowMessageBox(string Message)
+        {
+            System.Windows.Forms.MessageBox.Show(
+                PrintLine() + MethodBase.GetCurrentMethod().Name
+                + Environment.NewLine + Message);
+        }
+
+        #endregion
+
+        #region Private methods
+        private string PrintLine()
+        {
+            return "Hello from ";
+        }
+        #endregion
+    }
+}
---
+++ COMDLL/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("COMDLL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("COMDLL")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(true)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("993a99a8-3349-4371-8188-b059d7fb5340")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
